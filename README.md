e517cb1f6766385995844f9d178f6d52f99b75d2
bitnj <neilseas@gmail.com>
2017-06-16 15:16:34 -0400

    chore: first submission for review

--- README.md
+++ README.md
@@ -6,10 +6,23 @@ the findings.
 
 [Project Rubric](https://review.udacity.com/#!/rubrics/25/view)
 
-The following csv files are generated from the `prep_for_database.py` script
+### prep_for_database.py
+
+This file was provided from the course lessons and uses Python 2.7.  It was modified by completing
+the `shape_element` function.  Additional helper functions where added to
+support `shape_element`.
+
+The following csv files are generated as result of running the `prep_for_database.py` script.
 
 1. nodes.csv
 2. nodes_tags.csv
 3. ways.csv
 4. ways_nodes.csv
 5. ways_tags.csv
+
+These files are imported into a MySQL database containing tables with the same
+names as the csv files.  See the **open_map_project_schema.sql** file in this
+repository for details on the tables.
+
+See the **open_street_map_project.pdf** for a description of what tasks were
+performed.
--- prep_for_database.py
+++ prep_for_database.py
@@ -1,159 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""
-After auditing is complete the next step is to prepare the data to be inserted into a SQL database.
-To do so you will parse the elements in the OSM XML file, transforming them from document format to
-tabular format, thus making it possible to write to .csv files.  These csv files can then easily be
-imported to a SQL database as tables.
-
-The process for this transformation is as follows:
-- Use iterparse to iteratively step through each top level element in the XML
-- Shape each element into several data structures using a custom function
-- Utilize a schema and validation library to ensure the transformed data is in the correct format
-- Write each data structure to the appropriate .csv files
-
-We've already provided the code needed to load the data, perform iterative parsing and write the
-output to csv files. Your task is to complete the shape_element function that will transform each
-element into the correct format. To make this process easier we've already defined a schema (see
-the schema.py file in the last code tab) for the .csv files and the eventual tables. Using the 
-cerberus library we can validate the output against this schema to ensure it is correct.
-
-## Shape Element Function
-The function should take as input an iterparse Element object and return a dictionary.
-
-### If the element top level tag is "node":
-The dictionary returned should have the format {"node": .., "node_tags": ...}
-
-The "node" field should hold a dictionary of the following top level node attributes:
-- id
-- user
-- uid
-- version
-- lat
-- lon
-- timestamp
-- changeset
-All other attributes can be ignored
-
-The "node_tags" field should hold a list of dictionaries, one per secondary tag. Secondary tags are
-child tags of node which have the tag name/type: "tag". Each dictionary should have the following
-fields from the secondary tag attributes:
-- id: the top level node id attribute value
-- key: the full tag "k" attribute value if no colon is present or the characters after the colon if one is.
-- value: the tag "v" attribute value
-- type: either the characters before the colon in the tag "k" value or "regular" if a colon
-        is not present.
-
-Additionally,
-
-- if the tag "k" value contains problematic characters, the tag should be ignored
-- if the tag "k" value contains a ":" the characters before the ":" should be set as the tag type
-  and characters after the ":" should be set as the tag key
-- if there are additional ":" in the "k" value they and they should be ignored and kept as part of
-  the tag key. For example:
-
-  <tag k="addr:street:name" v="Lincoln"/>
-  should be turned into
-  {'id': 12345, 'key': 'street:name', 'value': 'Lincoln', 'type': 'addr'}
-
-- If a node has no secondary tags then the "node_tags" field should just contain an empty list.
-
-The final return value for a "node" element should look something like:
-
-{'node': {'id': 757860928,
-          'user': 'uboot',
-          'uid': 26299,
-       'version': '2',
-          'lat': 41.9747374,
-          'lon': -87.6920102,
-          'timestamp': '2010-07-22T16:16:51Z',
-      'changeset': 5288876},
- 'node_tags': [{'id': 757860928,
-                'key': 'amenity',
-                'value': 'fast_food',
-                'type': 'regular'},
-               {'id': 757860928,
-                'key': 'cuisine',
-                'value': 'sausage',
-                'type': 'regular'},
-               {'id': 757860928,
-                'key': 'name',
-                'value': "Shelly's Tasty Freeze",
-                'type': 'regular'}]}
-
-### If the element top level tag is "way":
-The dictionary should have the format {"way": ..., "way_tags": ..., "way_nodes": ...}
-
-The "way" field should hold a dictionary of the following top level way attributes:
-- id
--  user
-- uid
-- version
-- timestamp
-- changeset
-
-All other attributes can be ignored
-
-The "way_tags" field should again hold a list of dictionaries, following the exact same rules as
-for "node_tags".
-
-Additionally, the dictionary should have a field "way_nodes". "way_nodes" should hold a list of
-dictionaries, one for each nd child tag.  Each dictionary should have the fields:
-- id: the top level element (way) id
-- node_id: the ref attribute value of the nd tag
-- position: the index starting at 0 of the nd tag i.e. what order the nd tag appears within
-            the way element
-
-The final return value for a "way" element should look something like:
-
-{'way': {'id': 209809850,
-         'user': 'chicago-buildings',
-         'uid': 674454,
-         'version': '1',
-         'timestamp': '2013-03-13T15:58:04Z',
-         'changeset': 15353317},
- 'way_nodes': [{'id': 209809850, 'node_id': 2199822281, 'position': 0},
-               {'id': 209809850, 'node_id': 2199822390, 'position': 1},
-               {'id': 209809850, 'node_id': 2199822392, 'position': 2},
-               {'id': 209809850, 'node_id': 2199822369, 'position': 3},
-               {'id': 209809850, 'node_id': 2199822370, 'position': 4},
-               {'id': 209809850, 'node_id': 2199822284, 'position': 5},
-               {'id': 209809850, 'node_id': 2199822281, 'position': 6}],
- 'way_tags': [{'id': 209809850,
-               'key': 'housenumber',
-               'type': 'addr',
-               'value': '1412'},
-              {'id': 209809850,
-               'key': 'street',
-               'type': 'addr',
-               'value': 'West Lexington St.'},
-              {'id': 209809850,
-               'key': 'street:name',
-               'type': 'addr',
-               'value': 'Lexington'},
-              {'id': '209809850',
-               'key': 'street:prefix',
-               'type': 'addr',
-               'value': 'West'},
-              {'id': 209809850,
-               'key': 'street:type',
-               'type': 'addr',
-               'value': 'Street'},
-              {'id': 209809850,
-               'key': 'building',
-               'type': 'regular',
-               'value': 'yes'},
-              {'id': 209809850,
-               'key': 'levels',
-               'type': 'building',
-               'value': '1'},
-              {'id': 209809850,
-               'key': 'building_id',
-               'type': 'chicago',
-               'value': '366409'}]}
-"""
-
 import csv
 import codecs
 import pprint
@@ -164,7 +11,7 @@ import cerberus
 
 import schema
 
-OSM_PATH = "edinburgh_scotland_subset.osm"
+OSM_PATH = "edinburgh_scotland.osm"
 
 NODES_PATH = "nodes.csv"
 NODE_TAGS_PATH = "nodes_tags.csv"
@@ -173,7 +20,8 @@ WAY_NODES_PATH = "ways_nodes.csv"
 WAY_TAGS_PATH = "ways_tags.csv"
 
 LOWER_COLON = re.compile(r'^([a-z]|_)+:([a-z]|_)+')
-PROBLEMCHARS = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+# PROBLEMCHARS = re.compile(r'[=\+/&<>\'"\?%#$@\,\. \t\r\n]')
+PROBLEMCHARS = re.compile(r'[,]')
 
 SCHEMA = schema.schema
 
@@ -195,22 +43,18 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
     tags = []  # Handle secondary tags the same way for both node and way elements
 
     tag_detail = {}
-    # YOUR CODE HERE
+    
     if element.tag == 'node':
         node_attribs = populate_attributes(element, NODE_FIELDS)
         
-        tag_detail = populate_tags(element, node_attribs['id'])
-        if tag_detail:
-            tags.append(tag_detail.copy())
+        tags = populate_tags(element, node_attribs['id'])
         
         return {'node': node_attribs, 'node_tags': tags}
     
     elif element.tag == 'way':
         way_attribs = populate_attributes(element, WAY_FIELDS)
         
-        tag_detail = populate_tags(element, way_attribs['id'])
-        if tag_detail:
-            tags.append(tag_detail.copy())
+        tags = populate_tags(element, way_attribs['id'])
                 
         tag_detail = {}
         i = 0
@@ -231,6 +75,7 @@ def shape_element(element, node_attr_fields=NODE_FIELDS, way_attr_fields=WAY_FIE
 # ================================================== #
 # https://stackoverflow.com/questions/196345/how-to-check-if-a-string-in-python-is-in-ascii
 def is_ascii(my_string):
+    '''detects the presence of non-ascii characters in my_string'''
     if isinstance(my_string, unicode):
         try:
             my_string.encode('ascii')
@@ -247,6 +92,8 @@ def is_ascii(my_string):
 # iterates over the elements object and adds entries for any key that is in the
 # list of attr_fields that is passed in
 def populate_attributes(element, attr_fields):
+    '''iterates over the elements object and adds entries for any key that is in
+    the list of attr_fields that is passed in'''
     attrib_dict = {}
     for key, value in element.attrib.items():
         if is_ascii(key) and is_ascii(value):
@@ -256,8 +103,9 @@ def populate_attributes(element, attr_fields):
     return attrib_dict
 
 
-# iterates of the element object and adds entries for any tags
 def populate_tags(element, parent_id):
+    '''iterates over the element object and adds entries for any tags'''
+    tags = []
     tag_detail = {}
     for tag in element.findall('tag'):
         for key, value in tag.attrib.items():
@@ -272,19 +120,30 @@ def populate_tags(element, parent_id):
                         tag_detail['type'] = 'regular'
                     else:
                         if len(key_strs) == 3:
-                            tag_detail['key'] = key_strs[1] + ':' + key_strs[2]
-                            tag_detail['type'] = key_strs[0]
+                            tag_detail['key'] = key_strs[1]
+                            tag_detail['type'] = key_strs[0] + ':' + key_strs[2]
                         else:
                             tag_detail['key'] = key_strs[1]
                             tag_detail['type'] = key_strs[0]
                 elif key == 'v':
-                    tag_detail['value'] = value
-
-    return tag_detail
+                    # split the value on any ; in the string
+                    val_strs = value.split(';')[0]
+                    # currently only looks for , in the string to address issue
+                    # on import into MySQL.  Could be expanded to check for
+                    # additional problem characters
+                    m = PROBLEMCHARS.search(val_strs)
+                    if m:
+                        val_strs = val_strs.replace(',', '')
+                        tag_detail['value'] = val_strs
+                    else:
+                        tag_detail['value'] = val_strs
+                        
+        tags.append(tag_detail.copy())
+    return tags
 
 
 # ================================================== #
-#               Helper Functions                     #
+#               Provided Helper Functions            #
 # ================================================== #
 def get_element(osm_file, tags=('node', 'way', 'relation')):
     """Yield element if it is the right type of tag"""
@@ -301,13 +160,6 @@ def validate_element(element, validator, schema=SCHEMA):
     """""Modified to just return True or False"""
     return validator.validate(element, schema)
     
-    #if validator.validate(element, schema) is not True:
-    #    field, errors = next(validator.errors.iteritems())
-    #    message_string = "\nElement of type '{0}' has the following errors:\n{1}"
-    #    error_string = pprint.pformat(errors)
-        
-    #    raise Exception(message_string.format(field, error_string))
-
 
 class UnicodeDictWriter(csv.DictWriter, object):
     """Extend csv.DictWriter to handle Unicode input"""
